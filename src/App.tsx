263b41833ad63eda179d04f2fa759143a18f7600
jagadee-kudumula <jagadees.kudumula@gmail.com>
2025-09-25 20:57:27 +0530

    Initial build with basic pages

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import React, { use, useState, useEffect } from "react";
+import LandingPage from "./LandingPage";
+import HomeScreen from "./HomeScreen";
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+// Define a type for the user info
+type UserInfo = {
+  name: string;
+  email: string;
+};
+
+GoogleSignin.configure({
+  webClientId: '188805815138-jua9enfk6oslbtol9cm49lrb4c83c8h8.apps.googleusercontent.com',
+  offlineAccess: true,
+});
+
+const App: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState<UserInfo | null>(null);
+  const [loading, setLoading] = useState(true); // to check AsyncStorage first
+
+  useEffect(() => {
+    const loadData = async () => {
+      try {
+        const storedUser = await AsyncStorage.getItem("user");
+        const storedLoggedIn = await AsyncStorage.getItem("isLoggedIn");
+
+        if (storedUser && storedLoggedIn === "true") {
+          setUser(JSON.parse(storedUser));
+          setIsLoggedIn(true);
+        }
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadData();
+  }, []);
+
+  if (loading) {
+    return null; // or a splash/loading screen
+  }
+
+  return isLoggedIn && user ? (
+    <HomeScreen user={user} setIsLoggedIn={setIsLoggedIn} setUser={setUser} />
+  ) : (
+    <LandingPage setIsLoggedIn={setIsLoggedIn} setUser={setUser} />
+  );
+};
+
+export default App;
---
+++ src/LandingPage.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  StatusBar,
+  Image,
+  Alert,
+} from "react-native";
+import LinearGradient from "react-native-linear-gradient";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const amsLogo = require("../assets/images/rgukt_w.png");
+const googleLogo = require("../assets/images/google.png");
+
+type LandingPageProps = {
+  setIsLoggedIn: (value: boolean) => void;
+  setUser: (user: { name: string; email: string } | null) => void;
+};
+
+const LandingPage: React.FC<LandingPageProps> = ({ setIsLoggedIn, setUser }) => {
+  const handleGoogleLogin = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      // Extract user email(If userInfo, data and user are not null)
+      const userEmail = userInfo?.data?.user?.email || "";
+
+      // ✅ Restrict domain
+      if (!userEmail.endsWith("@rguktrkv.ac.in")) {
+        Alert.alert(
+          "Access Denied",
+          "Please use your College email"
+        );
+        await GoogleSignin.signOut();
+        setIsLoggedIn(false);
+        setUser(null);
+        AsyncStorage.removeItem("user");
+        AsyncStorage.removeItem("isLoggedIn");
+        return;
+      }
+
+      // Login successful → notify App
+      setIsLoggedIn(true);
+      setUser({ name: userInfo?.data?.user?.familyName || "", email: userEmail });
+      await AsyncStorage.setItem("user", JSON.stringify({ name: userInfo?.data?.user?.familyName || "", email: userEmail }));
+      await AsyncStorage.setItem("isLoggedIn", "true");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <LinearGradient
+      colors={["#600202", "#353535"]}
+      style={styles.container}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 1, y: 1 }}
+    >
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+      <Image source={amsLogo} style={styles.logoImage} />
+      <Text style={styles.welcomeText}>Welcome to AMS</Text>
+      <TouchableOpacity style={styles.loginButton} onPress={handleGoogleLogin}>
+        <Image source={googleLogo} style={styles.googleLogo} />
+        <Text style={styles.loginButtonText}>LogIn with Google</Text>
+      </TouchableOpacity>
+    </LinearGradient>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", justifyContent: "space-around", padding: 20 },
+  logoImage: { width: 150, height: 150, resizeMode: "contain", tintColor: "#FFF" },
+  welcomeText: { fontSize: 42, color: "#FFF", textAlign: "center", fontFamily: "QuicksandBold" },
+  loginButton: { flexDirection: "row", backgroundColor: "#FFF", padding: 12, paddingLeft: 25, paddingRight: 25, borderRadius: 30, alignItems: "center", justifyContent: "center" },
+  googleLogo: { width: 24, height: 24, marginRight: 15 },
+  loginButtonText: { fontSize: 30, color: "#444", fontFamily: "WorkSansBold" },
+});
+
+export default LandingPage;
